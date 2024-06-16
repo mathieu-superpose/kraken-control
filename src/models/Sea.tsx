14ed37889dc44480d7b490bb08a93b339a4a23b2
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-06-16 17:51:27 +0200

    add custom direction cursor

--- src/models/Sea.tsx
+++ src/models/Sea.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+
 function Sea() {
+  const [hovered, setHovered] = useState(false)
+
+  useEffect(() => {
+    document.body.style.cursor = hovered ? `url("/img/tentacle.png"), auto` : "auto"
+  }, [hovered])
+
   return (
     <group>
-      <mesh receiveShadow rotation-x={-Math.PI / 2} name="sea">
+      <mesh
+        receiveShadow
+        rotation-x={-Math.PI / 2}
+        name="sea"
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+      >
         <planeGeometry args={[30, 6, 4, 4]} />
         <meshStandardMaterial
           transparent
