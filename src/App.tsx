18b62c70106a3c74c0b5973235ed2650e5901028
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-06-16 16:25:38 +0200

    add camera perspective choice

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import { Canvas } from "@react-three/fiber"
 
 import Scene from "./Scene"
 
+const ORTHOGRAPHIC = false
+
 function App() {
+  if (ORTHOGRAPHIC)
+    return (
+      <Canvas shadows orthographic camera={{ position: [5, 5, 5], zoom: 100 }}>
+        <Scene />
+
+        <OrbitControls makeDefault />
+      </Canvas>
+    )
+
   return (
-    <Canvas shadows orthographic camera={{ position: [5, 5, 5], zoom: 100 }}>
+    <Canvas shadows camera={{ position: [5, 5, 5] }}>
       <Scene />
 
       <OrbitControls makeDefault />
